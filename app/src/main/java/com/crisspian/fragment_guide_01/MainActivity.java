d77b88bfd74fccdc625be245016b7f1e5acafb73
copito.123 <nicolspnicolsp@gmail.com>
2020-08-12 23:06:20 -0400

    Fragmento1

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,15 +1,44 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+        private ActivityMainBinding binging;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binging = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binging.getRoot());
+
+        binging.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            showFragment();
+            }
+        });
+
     }
+        private void showFragment() {
+            //generamos instancia del fragmento gracias al factorymetodo//
+
+            FirstFragment firstFragment = FirstFragment.newInstance("", "");
+            //obtener instancias de fragment manager//
+            FragmentManager fragmentManager =getSupportFragmentManager();
+            //Obtenenemos e instanciamos la
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            //añadir fragmento y lo asosiamos al contenedor donde se mostrara
+            fragmentTransaction.add(R.id.textF ,firstFragment).addToBackStack(null).commit();
+
+
+        }
 
 }
