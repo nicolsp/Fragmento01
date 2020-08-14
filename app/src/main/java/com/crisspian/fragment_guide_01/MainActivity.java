ec6fc24b20aa53733f24271fa9a0e9ef03e3b78d
copito.123 <nicolspnicolsp@gmail.com>
2020-08-13 22:52:49 -0400

    Fragmento1

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -12,6 +13,7 @@ import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
         private ActivityMainBinding binging;
+        private boolean isFragmentShow = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +24,18 @@ public class MainActivity extends AppCompatActivity {
         binging.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!isFragmentShow) {
+                    showFragment();
+                } else {
+                    closeFragment();
+                }
+
             showFragment();
             }
         });
 
 
-        
+
     }
         private void showFragment() {
             //generamos instancia del fragmento gracias al factorymetodo//
@@ -38,9 +46,22 @@ public class MainActivity extends AppCompatActivity {
             //Obtenenemos e instanciamos la
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             //añadir fragmento y lo asosiamos al contenedor donde se mostrara
-            fragmentTransaction.add(R.id.textF ,firstFragment).addToBackStack(null).commit();
+            fragmentTransaction.add(R.id.textF ,firstFragment).commit();
+            binging.button.setText("CLOSE");
+            isFragmentShow = true;
 
 
         }
 
+        private void closeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.textF);
+            if (fragment != null) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.remove(fragment).commit();
+            }
+            binging.button.setText("OPEN");
+            isFragmentShow = false;
+
+        }
 }
